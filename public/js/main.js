fa5748f934409cb56adc20283184e10afefa3e94
Leandro <Leandro>
2023-09-04 18:18:16 -0300

    correcion en main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,39 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
   //alert("dom listo");
+
+  async function peticionGet(con, met) {
+    try {
+      // Realiza la solicitud utilizando fetch
+
+      //const respuesta = await fetch("URL_DEL_RECURSO");
+      let headersList = {};
+
+      let respuesta = await fetch(
+        "http://localhost/MvcVideo/index.php?c=html_" + con + "&m=" + met,
+        {
+          method: "GET",
+          headers: headersList,
+        }
+      );
+
+      const contenidoHTML = await respuesta.text();
+
+      // Verifica si la solicitud fue exitosa (código de estado HTTP 200)
+      if (!respuesta.ok) {
+        throw new Error(`Error al realizar la solicitud: ${respuesta.status}`);
+      }
+
+      // Procesa la respuesta como texto HTML
+
+      // Hacer algo con el contenido HTML obtenido
+      console.log(contenidoHTML);
+    } catch (error) {
+      console.log(error);
+      console.error(`Se produjo un error: ${error.message}`);
+    }
+  }
+
   document.addEventListener("click", (event) => {
     alert("");
     const click = event.target;
@@ -19,5 +52,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     console.log("metodo");
     console.log(m);
     console.log("ir");
+    peticionGet(c, m);
   });
 });
