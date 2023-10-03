e8ae627caa23c80941b38ea0627452cc7aee01a1
Romina Celintano <romina.celintano.quinteros@gmail.com>
2023-10-03 01:26:15 -0300

    agregar funciones para leer el servidor

--- hora/hora.php
+++ hora/hora.php
@@ -11,75 +11,75 @@
   <!-- <script src="https://cdn.jsdelivr.net/npm/luxon@3.4.3/build/global/luxon.min.js"></script> -->
   <script src="../public/js/luxon.min.js"></script>
   <script>
-  window.addEventListener("DOMContentLoaded", (event) => {
-    console.log("hola");
+    window.addEventListener("DOMContentLoaded", (event) => {
+      console.log("hola");
 
-    function formatearAUTC(fecha) {
-      console.log("fechautc");
-      console.log(fecha);
-      const year = fecha.year;
-      const month = String(fecha.month + 1).padStart(2, '0');
-      const day = String(fecha.day).padStart(2, '0');
-      const hours = String(fecha.hour).padStart(2, '0');
-      const minutes = String(fecha.minute).padStart(2, '0');
-      const seconds = String(fecha.second).padStart(2, '0');
-      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    }
+      function formatearAUTC(fecha) {
+        console.log("fechautc");
+        console.log(fecha);
+        const year = fecha.year;
+        const month = String(fecha.month + 1).padStart(2, '0');
+        const day = String(fecha.day).padStart(2, '0');
+        const hours = String(fecha.hour).padStart(2, '0');
+        const minutes = String(fecha.minute).padStart(2, '0');
+        const seconds = String(fecha.second).padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+      }
 
-    function formatFecha(fechaUTC) {
+      function formatFecha(fechaUTC) {
 
-      const anio = fechaUTC.year;
-      const mes = fechaUTC.month;
-      const dia = fechaUTC.day;
-      const hora = fechaUTC.hour;
-      const minutos = fechaUTC.minute;
-      const segundos = fechaUTC.second;
+        const anio = fechaUTC.year;
+        const mes = fechaUTC.month;
+        const dia = fechaUTC.day;
+        const hora = fechaUTC.hour;
+        const minutos = fechaUTC.minute;
+        const segundos = fechaUTC.second;
 
-      console.log('Año:', anio);
-      console.log('Mes:', mes);
-      console.log('Día:', dia);
-      console.log('Hora:', hora);
-      console.log('Minutos:', minutos);
-      console.log('Segundos:', segundos);
-
-
-
-    }
+        console.log('Año:', anio);
+        console.log('Mes:', mes);
+        console.log('Día:', dia);
+        console.log('Hora:', hora);
+        console.log('Minutos:', minutos);
+        console.log('Segundos:', segundos);
 
 
 
-
-    //let horaaux = new Date("2023-09-25 22:55:53");
-    //convertUTCDateToLocalDate(horaaux);
-
-    var elementosHora = document.getElementsByClassName(".hora");
-
-    // elementosHora es una colección de elementos con la clase "hora"
-    // Puedes recorrerlos o acceder a elementos específicos por índice
-    //const moment = window.moment;
-    for (var i = 0; i < elementosHora.length; i++) {
-      //console.log(elementosHora[i].textContent);
-      let hora = elementosHora[i].textContent;
-      console.log("hora----");
-      console.log(hora);
-      //let horaC = convt(new Date(hora));
-      //console.log("elemento");
-      var DateTime = luxon.DateTime;
-      //let horaAH = String('2017-05-15 09:24:15');
-      let horaAH = String(hora);
-
-      let fechaUTC = DateTime.fromSQL(horaAH, {
-        zone: 'utc'
-      });
-      formatFecha(fechaUTC);
-      let fecha = formatearAUTC(fechaUTC);
-      console.log(fecha);
+      }
 
 
 
-      //console.log(fechaUTC.toISO());
-    }
-  });
+
+      //let horaaux = new Date("2023-09-25 22:55:53");
+      //convertUTCDateToLocalDate(horaaux);
+
+      var elementosHora = document.getElementsByClassName(".hora");
+
+      // elementosHora es una colección de elementos con la clase "hora"
+      // Puedes recorrerlos o acceder a elementos específicos por índice
+      //const moment = window.moment;
+      for (var i = 0; i < elementosHora.length; i++) {
+        //console.log(elementosHora[i].textContent);
+        let hora = elementosHora[i].textContent;
+        console.log("hora----");
+        console.log(hora);
+        //let horaC = convt(new Date(hora));
+        //console.log("elemento");
+        var DateTime = luxon.DateTime;
+        //let horaAH = String('2017-05-15 09:24:15');
+        let horaAH = String(hora);
+
+        let fechaUTC = DateTime.fromSQL(horaAH, {
+          zone: 'utc'
+        });
+        formatFecha(fechaUTC);
+        let fecha = formatearAUTC(fechaUTC.toLocal());
+        console.log(fecha);
+
+
+
+        //console.log(fechaUTC.toISO());
+      }
+    });
   </script>
   <?php
   //date_default_timezone_set('America/Montevideo');
--- js/fetch.js
+++ js/fetch.js
@@ -1,26 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
   const myForm = document.getElementById("myForm");
   const resultDiv = document.getElementById("result");
+  const urlBase = "http://localhost/MvcVideo/index.php";
+  const dataTable = document
+    .getElementById("dataTable")
+    .getElementsByTagName("tbody")[0];
 
   myForm.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const formData = new FormData(myForm);
 
+    //console.log(formData);
     try {
-      const response = await fetch("URL_DEL_SERVIDOR", {
+      let url = urlBase + "?c=php_Producto&m=listar";
+      const response = await fetch(url, {
         method: "POST",
         body: formData,
       });
-
+      //console.log(response);
       if (!response.ok) {
+        console.log("entro error");
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      const data = await response.json();
+      let data = await response.json();
+      //console.log("data");
+      console.log(data);
+      //genero la tabla
+      dataTable.innerHTML = "";
+      data.lista.forEach((item) => {
+        const row = dataTable.insertRow();
+        const idCell = row.insertCell(0);
+        const codigoCell = row.insertCell(1);
+        const descripcionCell = row.insertCell(2);
 
+        idCell.textContent = item.id;
+        codigoCell.textContent = item.codigo;
+        descripcionCell.textContent = item.descripcion;
+      });
       // Haz algo con la respuesta JSON
-      resultDiv.innerHTML = JSON.stringify(data);
+      //resultDiv.innerHTML = JSON.stringify(data);
     } catch (error) {
       console.error("Fetch error:", error);
     }
